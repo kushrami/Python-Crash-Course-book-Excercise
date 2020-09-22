f3c394a4bbfc06b8253f4c5db6455305cc976c73
kushrami <kushrami16@gmail.com>
2020-09-22 11:01:52 +0530

    Excersice 1.2 of chepter 1

---
+++ Excersice_1_2.py
@@ -0,0 +1,6 @@
+'''print statement without error. we need parenthesis. we need perticuler type allignment in 
+perenthesis. No issues found in below statement.'''
+print("Hello! World.")
+
+'''print statement will generate error of syntaxerror because we miss spelled prnt, also we did not put a string in "" .'''
+print(Hello! World)
