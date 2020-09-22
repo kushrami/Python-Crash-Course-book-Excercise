a3006ff51b53f1e6616e81087f4425f46d9f5efc
kush rami <kushrami16@gmail.com>
2020-09-22 11:38:49 +0530

    Update Excersice_1_1.py

--- Excersice_1_1.py
+++ Excersice_1_1.py
@@ -1,2 +1,2 @@
-# this excersice was about visiting python.org. i liked some topics like oops, data structures
-#algos, AI , ML. Thats it completed this excersice.
+# this excercise was about visiting python.org. i liked some topics like oops, data structures
+#algos, AI , ML. Thats it completed this excercise.
