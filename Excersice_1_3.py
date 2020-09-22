f19506a61fa788380de43dcf40846372931afe21
kushrami <kushrami16@gmail.com>
2020-09-22 11:02:34 +0530

    Excersice 1.3 of chepter 1

---
+++ Excersice_1_3.py
@@ -0,0 +1,4 @@
+#if i know programming, i would love to build an apps for desktops. and tabs. 
+#some great apps for productivity, and content management,
+#also i have some VLSI knoweledge so i will build some opensource app to help community to 
+# achive some basic devlopement without any payable software.
