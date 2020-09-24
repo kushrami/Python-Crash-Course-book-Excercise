318f2f9c07422d7c89bb10bf5a027c8acc45fbbe
kushrami <kushrami16@gmail.com>
2020-09-24 11:56:23 +0530

    Excercise 2.2 of chepter 2.

---
+++ Excercise_2_2.py
@@ -0,0 +1,8 @@
+#this prigram is simple massage print task. while chaging in between variable value.
+message = "Belive In yourself!"
+
+print(message)
+
+message = "That means, convince your mind that you can do, what you dream or belive."
+
+print(message)
