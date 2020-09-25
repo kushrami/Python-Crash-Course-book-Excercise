75268e68dda85c4546c0534b4b7ca9af8a88c7be
kushrami <kushrami16@gmail.com>
2020-09-25 11:35:20 +0530

    Excercise 2.4 of chepter 2

---
+++ Excercise_2_4.py
@@ -0,0 +1,8 @@
+#Name Cases.
+
+Personal_Name = "joey Tribionny"
+
+print("Person's name in lower case: " + Personal_Name.lower())
+print("Person's name in upper case: " + Personal_Name.upper())
+print("Person's name in title case: " + Personal_Name.title())
+
