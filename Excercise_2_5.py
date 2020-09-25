aa4dc1be4d8daee009695a2eb1d3680718659edf
kushrami <kushrami16@gmail.com>
2020-09-25 11:35:56 +0530

    Excercise 2.5 of chepter 2

---
+++ Excercise_2_5.py
@@ -0,0 +1,6 @@
+#Famous Quote:
+
+
+Quote = "Hardwork, dedication, persevrance, disipline, etc all is required. But what matters the most in life is HONESTY."
+
+print("M. S. Dhoni once said, \"" + Quote + "\"")
