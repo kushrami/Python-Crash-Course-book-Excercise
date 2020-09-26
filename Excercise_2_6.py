0cfe6cb9f8c3729eb50e78724a25f1cb0fa29cc2
kushrami <kushrami16@gmail.com>
2020-09-26 20:09:38 +0530

    Excercise 2.6 of chepter 2

---
+++ Excercise_2_6.py
@@ -0,0 +1,7 @@
+#Famous Quote:
+
+Famous_person = "M. S. Dhoni"
+
+Quote = "Hardwork, dedication, persevrance, disipline, etc all is required. But what matters the most in life is HONESTY."
+
+print(Famous_person+" once said, \"" + Quote + "\"")
