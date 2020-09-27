bb43d3bd467a6393a1a654a16e55b67ac8e1543f
kushrami <kushrami16@gmail.com>
2020-09-27 11:06:09 +0530

    Excersice 2.7 of chepter 2.

---
+++ Excercise_2_7.py
@@ -0,0 +1,9 @@
+#Strinping Names:
+
+Person_name = " Chandler Bing "
+
+print("Person Name with tab space :\t"+Person_name)
+print("Person Name in new line :\n"+Person_name)
+print("Person Name with space removed from left side :"+Person_name.lstrip())
+print("Person Name with space removed from right side :"+Person_name.rstrip())
+print("Person Name with space removed from both sides:"+Person_name.strip())
