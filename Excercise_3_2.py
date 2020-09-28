6dfe86d231e448e2221f15cab297f51bfea50452
kushrami <kushrami16@gmail.com>
2020-09-28 11:45:47 +0530

    Excercise 3.2 of chepter 3.

---
+++ Excercise_3_2.py
@@ -0,0 +1,10 @@
+#Greetings:
+
+names = ['tony','steve','thor','peter']
+
+message = " , You are awesome!"
+
+print(names[0]+message)
+print(names[1]+message)
+print(names[2]+message)
+print(names[3]+message)
