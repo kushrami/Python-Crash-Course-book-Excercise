adc73130a99b50e6d5c91a48f142b1d4eaf9df90
kushrami <kushrami16@gmail.com>
2020-09-29 14:39:50 +0530

    Excercise 3.3 of chepter 3.

---
+++ Excercise_3_3.py
@@ -0,0 +1,7 @@
+#Your Own list.
+
+List_of_transportation = ["car","bike","truck"]
+
+print("I just love to ride a "+List_of_transportation[1]+".")
+print("But i also love to sit in a "+List_of_transportation[0]+" comfortably and drive.")
+print("I also play simulator games of "+List_of_transportation[2]+".")
