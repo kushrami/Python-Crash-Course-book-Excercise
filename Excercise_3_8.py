456e0eaadcc9167a6f8073a20b6f5cd3b4c8145f
kushrami <kushrami16@gmail.com>
2020-10-02 11:57:25 +0530

    Excercise 3.8 of chepter 3.

---
+++ Excercise_3_8.py
@@ -0,0 +1,17 @@
+#seeing the world:
+ 
+locations = ['Dubai','Thailand','everest','paris','newyork']
+print(locations)
+print("Sorted locations = ",sorted(locations))
+print(locations)
+print("Reversed Sorted locations = ",sorted(locations,reverse=True))
+print(locations)
+locations.reverse()
+print("Reversed original locations = ",locations)
+locations.reverse()
+print("Original locations = ",locations)
+locations.sort()
+print("Sorted locations",locations)
+locations.reverse()
+print("Reverse of sorted location",locations)
+
