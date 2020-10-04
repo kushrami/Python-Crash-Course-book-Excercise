90462d9f2c8ff7abc415b3514fea6c748315a3f0
kushrami <kushrami16@gmail.com>
2020-10-04 12:40:03 +0530

    Excercise 4.1 of chepter 4.

---
+++ Excercise_4_1.py
@@ -0,0 +1,10 @@
+#pizzas:
+
+pizzas = ['margerita','thincrust','peripiri']
+for pizza in pizzas:
+    print(pizza)
+
+for pizza in pizzas:
+    print("i like ",pizza,"so much!")
+
+print("so like, i love pizza.")
