f6061eaf4155d0c779ea61eba48fa1746bd62c46
kushrami <kushrami16@gmail.com>
2020-10-08 12:04:42 +0530

    Exercise 4.15 of chepter 4

---
+++ Excercise_4_15.py
@@ -0,0 +1,7 @@
+#Code review
+
+# i am using vsd for code editing so i already have that tab converted to four tabs.
+
+#already in 80 charactors limit
+
+#Mostly never used blank lines without any intention
