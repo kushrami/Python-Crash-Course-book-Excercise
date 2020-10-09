05a86b5cef5a86ce879961104a77e1f730818c4d
kushrami <kushrami16@gmail.com>
2020-10-09 11:06:50 +0530

    Exercise 5.1 of chepter 5.

---
+++ Exercise_5_1.py
@@ -0,0 +1,18 @@
+#Conditional Tests
+
+time = 'morning'
+print("is it morning? i would say True")
+print(time == 'morning')
+print("is it not morning? i would say False")
+print(time != 'morning')
+print("is it evening then? i would  say False")
+print(time == 'evening')
+time = 'afternoon'
+print("if its not evening then it may be afternoon?  I would say lets check it.")
+print(time == 'afternoon')
+print("so its not morning? i would say True")
+print(time != 'morning')
+print("is morning is passed? i would say True")
+print(time < 'morning')
+print("Is it evening yet? I would say False")
+print(time > "evening")
