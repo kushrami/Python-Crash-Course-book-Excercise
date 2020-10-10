b9454a1154f6e5418858ee1548308dc908fcb978
kushrami <kushrami16@gmail.com>
2020-10-10 18:57:07 +0530

    Execise 5.4 of chepter 5.

---
+++ Exercise_5_4.py
@@ -0,0 +1,6 @@
+#Alian Colors #2
+alian_color = 'green'
+if alian_color == 'green' or alian_color == 'yellow' or alian_color == 'red':
+    print("You just earned 5 points.")
+else:
+    print("You just earned 10 points.")
