3c2ac9f410ef864b76bcafee783bb59504188d09
kushrami <kushrami16@gmail.com>
2020-10-12 10:40:03 +0530

    Execise 5.5 of chepter 5.

---
+++ Exercise_5_5.py
@@ -0,0 +1,24 @@
+#Alian Colors #3
+alian_color = 'green'
+if alian_color == 'green':
+    print("You just earned 5 points.")
+elif alian_color == 'yellow':
+    print("You just earned 10 points.")
+elif alian_color == 'red':
+    print("You just earned 15 points.")    
+
+alian_color = 'yellow'
+if alian_color == 'green':
+    print("You just earned 5 points.")
+elif alian_color == 'yellow':
+    print("You just earned 10 points.")
+elif alian_color == 'red':
+    print("You just earned 15 points.") 
+
+alian_color = 'red'
+if alian_color == 'green':
+    print("You just earned 5 points.")
+elif alian_color == 'yellow':
+    print("You just earned 10 points.")
+elif alian_color == 'red':
+    print("You just earned 15 points.") 
