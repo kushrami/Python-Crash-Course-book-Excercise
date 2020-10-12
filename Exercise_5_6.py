752f48e63aeb574d203bb846fdf9cd1005ce1836
kushrami <kushrami16@gmail.com>
2020-10-12 10:40:52 +0530

    Execise 5.6 of chepter 5.

---
+++ Exercise_5_6.py
@@ -0,0 +1,21 @@
+#Life stages
+age = 25
+
+if age < 2:
+    print("You are a baby!")
+
+elif age >= 2 and age < 4:
+    print("You are toddler.")
+
+elif age >= 4 and age < 13:
+    print("You are kid.")
+
+elif age >=13 and age < 20:
+    print("You are teenager.")
+
+elif age >=20 and age < 65:
+    print("You are adult.")
+
+else :
+    print("You are older.")    
+
