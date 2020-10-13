8bab7a80ed9a0ec6605704b3e1cba18c55e04998
kushrami <kushrami16@gmail.com>
2020-10-13 21:22:06 +0530

    Execise 5.7 of chepter 5.

---
+++ Exercise_5_7.py
@@ -0,0 +1,18 @@
+#favorite fruit:
+
+favorite_fruits = ['banana', 'watermelon', 'apple', 'papaya']
+
+if 'banana' in favorite_fruits:
+    print("you really like bananas.")
+
+if 'carrot' in favorite_fruits:
+    print()
+
+if 'apple' in favorite_fruits:
+    print("you really like apples.")
+
+if 'kiwi' in favorite_fruits:
+    print()
+
+if 'watermelon' in favorite_fruits:
+    print("you really like watermelons.")
