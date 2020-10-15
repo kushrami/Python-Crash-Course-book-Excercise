8a2fbd83ac008e5131971d69bb7b5f7cd85fd406
kushrami <kushrami16@gmail.com>
2020-10-15 17:40:25 +0530

    Execise 5.8 of chepter 5.

---
+++ Exercise_5_8.py
@@ -0,0 +1,8 @@
+#hello admin
+
+usernames = ['aes','bes','ces','des','ees','admin']
+for user in usernames:
+        if user == 'admin':
+            print("Hello, admin! Would you like to see a status report?")
+        else :
+            print("hello",user,". Thank you for logging in.")
