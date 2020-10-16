2799378dfdf3c791207d540ad03cfada6cccabac
kushrami <kushrami16@gmail.com>
2020-10-16 11:49:58 +0530

    Exercise 5.12 of chepter 5.

---
+++ Exercise_5_12.py
@@ -0,0 +1,4 @@
+#styling if statements:
+
+#mostly i write good if styled statements, also python has identation that gives advantage.
+#but that space between : and else keyword is good for reading the code,
