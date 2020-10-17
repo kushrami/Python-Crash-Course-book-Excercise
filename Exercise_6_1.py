defe49155086fafde55f5c33b71af27342da6a0a
kushrami <kushrami16@gmail.com>
2020-10-17 11:54:07 +0530

    Exercise 6.1 of chepter 6.

---
+++ Exercise_6_1.py
@@ -0,0 +1,13 @@
+#Person:
+
+person = {
+    'firstname' : 'Tony',
+    'lastname' : 'stark',
+    'age' : 32,
+    'city' : 'newyork'
+    }
+
+print(person['firstname'])
+print(person['lastname'])
+print(person['age'])
+print(person['city'])
