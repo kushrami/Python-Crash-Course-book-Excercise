c7101c8062fd5cb087682a157424f34d07ab5400
kushrami <kushrami16@gmail.com>
2020-10-18 11:11:50 +0530

    Exercise 6.2 of chepter 6.

---
+++ Exercise_6_2.py
@@ -0,0 +1,16 @@
+#Favorite Numbers:
+
+PeopleFavNumbers ={
+    'tony' : 4,
+    'thor' : 5,
+    'steve' : 7,
+    'peter' : 6,
+    'nessa' : 8
+}
+
+print("Tony's fav number is :", PeopleFavNumbers['tony'])
+print("Thor's fav number is :", PeopleFavNumbers['thor'])
+print("Steve's fav number is :", PeopleFavNumbers['steve'])
+print("Peter's fav number is :", PeopleFavNumbers['peter'])
+print("nessa's fav number is :", PeopleFavNumbers['nessa'])
+
