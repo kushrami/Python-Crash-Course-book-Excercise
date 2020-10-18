470eb4aa37b86c0138aca6fadfd8718d30a61c20
kushrami <kushrami16@gmail.com>
2020-10-18 11:12:16 +0530

    Exercise 6.3 of chepter 6.

---
+++ Exercise_6_3.py
@@ -0,0 +1,19 @@
+#Glossary:
+Programming_words = {
+    'method' : 'function or process to do something in a program',
+    'list' : 'Collection of  values which can be accessible through index starting from 0.',
+    'dictionary' : 'Collection of values not stored with index but with perticular key',
+    'variable' : 'Storage for value which keeps changing',
+    'data type' : 'Perticular indentification for diffrent values'
+    }
+
+print("The meaning of method in programming is :")
+print("\t"+Programming_words['method'])
+print("The meaning of list in programming is :")
+print("\t"+Programming_words['list'])
+print("The meaning of dictionary in programming is :")
+print("\t"+Programming_words['dictionary'])
+print("The meaning of variable in programming is :")
+print("\t"+Programming_words['variable'])
+print("The meaning of data type in programming is :")
+print("\t"+Programming_words['data type'])
