7340a6c222585c09fdde2827b439312a1bfb010b
kushrami <kushrami16@gmail.com>
2020-10-18 11:12:38 +0530

    Exercise 6.4 of chepter 6.

---
+++ Exercise_6_4.py
@@ -0,0 +1,16 @@
+#Glossary 2:
+Programming_words = {
+    'method' : 'function or process to do something in a program',
+    'list' : 'Collection of  values which can be accessible through index starting from 0.',
+    'dictionary' : 'Collection of values not stored with index but with perticular key',
+    'variable' : 'Storage for value which keeps changing',
+    'data type' : 'Perticular indentification for diffrent values',
+    'keywords' : 'the words that are reserved for a language',
+    'username' : 'perticuler person\'s name for system to rememmber',
+    'error' : 'result of typo mistake to any thing worng in program',
+    'memory' : 'Real computer storage for saving any information',
+    'warning' : 'Does not stop program from execution but requires attention to solve the problem',
+    'loop' : 'Repetation of things or getting executing same everytime'
+    }
+for word, meaning in Programming_words.items():
+    print("The meaning of "+word+" in programming is : \n\t"+meaning)
