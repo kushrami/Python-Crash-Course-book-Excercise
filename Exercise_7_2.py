3ebb24eb545e11f44de4755e69f3707aac4a5e93
kushrami <kushrami16@gmail.com>
2020-10-22 23:14:13 +0530

    Exercise 7.2 of chepter 7.

---
+++ Exercise_7_2.py
@@ -0,0 +1,7 @@
+#Restaurant seating:
+number_of_people = input("how many people are in their dinner group : ")
+
+if int(number_of_people) > 8:
+    print("You have to wait for the table.")
+else:
+    print("Yes we have table.")
