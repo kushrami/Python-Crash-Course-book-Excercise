21a3be24d9b4cd9e88a1576a8f02e69ac303219c
kush rami <kushrami16@gmail.com>
2020-11-26 12:19:06 +0530

    Exercise 4.14 of chepter 4.

--- Excercise_4_14.py
+++
@@ -1,3 +0,0 @@
-#PEP
-
-# you can see whats is there to PEP 8 style guide at https://python.org/dev/peps/pep-0008/.
---
+++ Exercise_4_14.py
@@ -0,0 +1,3 @@
+#PEP
+
+# you can see whats is there to PEP 8 style guide at https://python.org/dev/peps/pep-0008/.
