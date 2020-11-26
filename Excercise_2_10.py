70b8943508b541c1642d4e9a6be4b1c2357a9f44
kush rami <kushrami16@gmail.com>
2020-11-26 12:00:23 +0530

    Update and rename Excercise_2_10.py to Exercise_2_10.py

--- Excercise_2_10.py
+++
@@ -1,9 +0,0 @@
-#Adding Comments
-
-#I Have added Comments in most of My Programs.From now on, I need to add below details
-
-#Author: kushrami
-#date : 15 sept 2020
-
-'''This excersice is about commenting stuff. you can use # for one line commnents.
-also for paragraph you can use this doc string of three single quotes. '''
