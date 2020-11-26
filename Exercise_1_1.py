a3a85c181920989429321e7f695728e3bdff98be
kush rami <kushrami16@gmail.com>
2020-11-26 12:25:16 +0530

    Exercise 1.1 of chepter 1.

--- Exercise_1_1.py
+++ Exercise_1_1.py
@@ -1,2 +1,2 @@
 # this exercise was about visiting python.org. i liked some topics like oops, data structures
-#algos, AI , ML. Thats it completed this exercise.
+#algos, AI , ML. Thats it completed this exercise. 
