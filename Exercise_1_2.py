7d392f618358d2ee89e47737b512398023367034
kush rami <kushrami16@gmail.com>
2020-11-26 12:25:48 +0530

    Exercise 1.2 of chepter 1.

--- Exercise_1_2.py
+++ Exercise_1_2.py
@@ -2,5 +2,5 @@
 perenthesis. No issues found in below statement.'''
 print("Hello! World.")
 
-'''print statement will generate error of syntaxerror because we miss spelled prnt, also we did not put a string in "" .'''
+'''print statement will generate error of syntaxerror because we miss spelled prnt, also we did not put a string in "" . '''
 print(Hello! World)
