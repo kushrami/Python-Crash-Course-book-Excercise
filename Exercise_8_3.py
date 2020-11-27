360e5d9aa0ee295b1be994972db8c1036ae1c17f
kushrami <kushrami16@gmail.com>
2020-11-27 14:25:59 +0530

    Exercise 8.3 of chepter 8.

---
+++ Exercise_8_3.py
@@ -0,0 +1,9 @@
+#T-shirt:
+
+def make_tshirt(size,text):
+    print("The size of tshirt is :",size)
+    print("And It says :",text)
+
+make_tshirt(42,"I don't know.")
+
+make_tshirt(size=34,text='jsdfhskdgn')
