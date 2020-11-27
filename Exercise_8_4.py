00c475d20502148b1ef9bf3c0d9cfa03eb40dbd1
kushrami <kushrami16@gmail.com>
2020-11-27 14:26:21 +0530

    Exercise 8.4 of chepter 8.

---
+++ Exercise_8_4.py
@@ -0,0 +1,11 @@
+#Large Shirts:  
+
+def make_tshirt(size='L',text='I love python'):
+    print("The size of tshirt is :",size)
+    print("And It says :",text)
+
+make_tshirt('L')
+make_tshirt('M')
+
+make_tshirt(text='jsdfhskdgn')
+
