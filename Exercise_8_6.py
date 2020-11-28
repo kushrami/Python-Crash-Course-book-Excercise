6f860f9c8980596b888f16ff11ba516502c382dc
kushrami <kushrami16@gmail.com>
2020-11-28 11:49:51 +0530

    Exercise 8.6 of chepter 8.

---
+++ Exercise_8_6.py
@@ -0,0 +1,7 @@
+#city names:
+
+def city_country(name, country='india'):
+    print("\""+name.title()+", "+country.title()+"\"")
+city_country('newyork','USA')
+city_country('mumbai')
+city_country('paris','france')
