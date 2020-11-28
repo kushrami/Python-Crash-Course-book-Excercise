1d9291091310bf4e5bf4530012c37a621a7c9982
kushrami <kushrami16@gmail.com>
2020-11-28 11:49:15 +0530

    Exercise 8.5 of chepter 8.

---
+++ Exercise_8_5.py
@@ -0,0 +1,7 @@
+#Cities:
+
+def describe_city(name, country='india'):
+    print(name,"is in",country+".")
+describe_city('newyork','USA')
+describe_city('mumbai')
+describe_city(name = 'paris',country='France')
