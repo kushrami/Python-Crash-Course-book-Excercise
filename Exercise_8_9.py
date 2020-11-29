88f54b0159fb8676b336d5c2e9b7dd2d922913d3
kushrami <kushrami16@gmail.com>
2020-11-29 12:11:59 +0530

    Exercise 8.9 of chepter 8.

---
+++ Exercise_8_9.py
@@ -0,0 +1,9 @@
+#magicians:
+
+listOfMagicians = ['Dynamo','david blain','karan singh magic']
+
+def show_magicians(listOfMagicians):
+    for magician in listOfMagicians:
+        print(magician)
+
+show_magicians(listOfMagicians)
