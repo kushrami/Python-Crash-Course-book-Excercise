47d393c691ad96b1e0aa5b7288897fd109cb5b42
kushrami <kushrami16@gmail.com>
2020-12-04 11:57:47 +0530

    Exercise 9.15 of chepter 9.

---
+++ Exercise_9_15.py
@@ -0,0 +1,3 @@
+#Good way to look at modules is http://pymotw.com/ and look at the table of contents. Find a module that
+#looks interesting to you and read about it, or explore the documentation of
+#the collections and random modules.
