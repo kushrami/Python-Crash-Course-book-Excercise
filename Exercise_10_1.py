9ef8d3d8970282982f92cd7829d26f12c453e873
kushrami <kushrami16@gmail.com>
2020-12-05 11:17:05 +0530

    Exercise 10.1 of chepter 10.

---
+++ Exercise_10_1.py
@@ -0,0 +1,15 @@
+#learning python:
+with open('learning_python.txt') as file_object:
+    content = file_object.read()
+    print(content)
+
+with open('learning_python.txt') as file_object:
+    lines = file_object.readlines()
+    for line in lines:
+        print(line)
+
+with open('learning_python.txt') as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+       print(line)
