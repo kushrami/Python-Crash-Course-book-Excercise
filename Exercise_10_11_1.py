92c1bde09f9e163ab2fc463266330e66a9568c57
kush rami <kushrami16@gmail.com>
2020-12-11 16:37:37 +0530

    Exercise 10.11 of chepter 10.

--- Exercise_10_11_1.py
+++ Exercise_10_11_1.py
@@ -1,10 +1,10 @@
 #Favorite Number:
 import json
 
-with open("Excercise_10_11.json") as fileobject:
+with open("Exercise_10_11.json") as fileobject:
     number = json.load(fileobject)
 print("Your favorite number is : ",number)
 
-with open("Excercise_10_11.json") as fileobject:
+with open("Exercise_10_11.json") as fileobject:
     number = json.load(fileobject)
 print("Your favorite number is : ",number)
